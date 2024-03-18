781967259453e0be653bd73ea04b78773850a21b
Mohamed Fathy <132439198+Mohamedfathy90@users.noreply.github.com>
2024-03-18 02:19:52 +0200

    18f18ff8f

--- storage/framework/views/27ab57eea565493a005e77126018b5e8.php
+++ storage/framework/views/27ab57eea565493a005e77126018b5e8.php
@@ -35,21 +35,42 @@
                         </div>
 
                         
-                        <?php if(session()->has('message')): ?>
-                            <div class="bg-yellow-100 ltr:border-l-4 rtl:border-r-4 border-yellow-400 p-4 mb-6">
-                                <div class="flex">
-                                    <div class="flex-shrink-0">
-                                        <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"> <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/> </svg>
-                                    </div>
-                                    <div class="ltr:ml-3 rtl:mr-3">
-                                        <p class="text-sm text-yellow-700 font-medium">
-                                            <?php echo e(session()->get('message'), false); ?>
-
-                                        </p>
+                            <?php if(session()->has('success')): ?>
+                                <div class="p-4 relative flex bg-emerald-50 dark:bg-emerald-500 text-emerald-500 dark:text-emerald-50 text-xs font-semibold rounded-lg mb-6">
+                                    <div class="flex items-center">
+                                        <span class="text-2xl text-emerald-400 dark:text-emerald-50">
+                                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+                                            </svg>
+                                        </span>
+                                        <div class="ltr:ml-2 rtl:mr-2"><?php echo e(session()->get('success'), false); ?></div>
                                     </div>
                                 </div>
-                            </div>
-                        <?php endif; ?>
+                            <?php endif; ?>
+
+                            
+                            <?php if(session()->has('error')): ?>
+                                <div class="p-4 relative flex bg-red-50 dark:bg-red-500 text-red-500 dark:text-red-50 text-xs font-semibold rounded-lg mb-6">
+                                    <div class="flex items-center">
+                                        <span class="text-2xl text-red-400 dark:text-red-50">
+                                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
+                                        </span>
+                                        <div class="ltr:ml-2 rtl:mr-2"><?php echo e(session()->get('error'), false); ?></div>
+                                    </div>
+                                </div>
+                            <?php endif; ?>
+
+                            
+                            <?php if(session()->has('warning')): ?>
+                                <div class="p-4 relative flex bg-amber-50 dark:bg-amber-500 text-amber-500 dark:text-amber-50 text-xs font-semibold rounded-lg mb-6">
+                                    <div class="flex items-center">
+                                        <span class="text-2xl text-amber-400 dark:text-amber-50">
+                                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 20 20" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path></svg>
+                                        </span>
+                                        <div class="ltr:ml-2 rtl:mr-2"><?php echo e(session()->get('warning'), false); ?></div>
+                                    </div>
+                                </div>
+                            <?php endif; ?>
                         
                         
                         <div class="w-full mb-6">
