a87012befc91cf44ea05e24416422c487b29fb4a
Mohamed Fathy <132439198+Mohamedfathy90@users.noreply.github.com>
2024-04-23 01:18:06 +0200

    update rules

--- app/Http/Validators/API/SetupValidator.php
+++ app/Http/Validators/API/SetupValidator.php
@@ -21,43 +21,47 @@ class SetupValidator
 
            // Set rules
          $rules    = [
-            'username' => ['required', 'max:60', 'min:3', Rule::unique('users')->ignore($request->user_id), new UsernameRule()],
-            'email'    => ['required', 'max:60', 'min:6', Rule::unique('users')->ignore($request->user_id)],
-            'fullname' => 'required|max:60',
-            'country'  => 'nullable|exists:countries,id',
-            'city'     => 'nullable|max:60',
-            'phone'    => 'required|numeric',
+            'username'     => ['required', 'max:60', 'min:3', Rule::unique('users')->ignore($request->user_id), new UsernameRule()],
+            'email'        => ['required', 'max:60', 'min:6', Rule::unique('users')->ignore($request->user_id)],
+            'fullname'     => 'required|max:60',
+            'country'      => 'required|exists:countries,id',
+            'city'         => 'nullable|max:60',
+            'phone'        => 'required|numeric',
+            'timezone'     => 'required' ,
+            'description'  => 'required' ,
         ];
 
         // Set errors messages
         $messages = [
-            'username.required' => __('username is required'),
-            'username.max'      => __('messages.t_validator_max', ['max' => 60]),
-            'username.min'      => __('messages.t_validator_min', ['min' => 3]),
-            'username.unique'   => __('username is already taken'),
-            'email.required'    => __('Email address is required'),
-            'email.max'         => __('messages.t_validator_max', ['max' => 60]),
-            'email.min'         => __('messages.t_validator_min', ['min' => 6]),
-            'email.unique'      => __('Email address is already taken'),
-            'fullname.required' => __('Fullname is required'),
-            'fullname.max'      => __('messages.t_validator_max', ['max' => 60]),
-            'country.exists'    => __('messages.t_validator_exists'),
-            'city.max'          => __('messages.t_validator_max', ['max' => 60]),
-            'timezone.max'      => __('messages.t_validator_max', ['max' => 60]),
-            'timezone.required' => __('messages.t_validator_required'),
-            'phone.required'        => __('Phone Number is required'),
-            'phone.numeric'         => __('Phone Number should be numeric value'),
+            'username.required'      => __('username is required'),
+            'username.max'           => __('messages.t_validator_max', ['max' => 60]),
+            'username.min'           => __('messages.t_validator_min', ['min' => 3]),
+            'username.unique'        => __('username is already taken'),
+            'email.required'         => __('Email address is required'),
+            'email.max'              => __('messages.t_validator_max', ['max' => 60]),
+            'email.min'              => __('messages.t_validator_min', ['min' => 6]),
+            'email.unique'           => __('Email address is already taken'),
+            'fullname.required'      => __('Fullname is required'),
+            'fullname.max'           => __('messages.t_validator_max', ['max' => 60]),
+            'country.exists'         => __('messages.t_validator_exists'),
+            'city.max'               => __('messages.t_validator_max', ['max' => 60]),
+            'timezone.max'           => __('messages.t_validator_max', ['max' => 60]),
+            'timezone.required'      => __('Timezone is required'),
+            'phone.required'         => __('Phone Number is required'),
+            'phone.numeric'          => __('Phone Number should be numeric value'),
 
         ];
 
         // Set data to validate
         $data     = [
-            'email'    => $request->email,
-            'username' => $request->username,
-            'fullname' => $request->fullname,
-            'country'  => $request->country,
-            'city'     => $request->city,
-            'phone'    => $request->phone,
+            'email'          => $request->email,
+            'username'       => $request->username,
+            'fullname'       => $request->fullname,
+            'country'        => $request->country,
+            'city'           => $request->city,
+            'phone'          => $request->phone,
+            'timezone'       => $request->timezone,
+            'description'    => $request->description,
         ];
 
         // Validate data
